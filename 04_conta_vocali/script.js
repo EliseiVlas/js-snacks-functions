5cece2a60438d2415da7825e5110f1909338554f
Elisei <eliseivlas.002@gmail.com>
2025-01-09 20:11:34 +0100

    consegna esercizio completo

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -16,9 +16,6 @@ function primaParola(stringa){
         numeriVocali++
 
         vocaliTrovate.push(stringa[i])
-       
-        
-        
        }
        
     }
@@ -26,10 +23,33 @@ function primaParola(stringa){
     return numeriVocali + " " + vocaliTrovate;
     
 }
+// funzione arrai
+const primaParola2 = stringa2 => {
+    
+    const lettere2 = ["a", "e", "i", "o", "u"]
+    
+    let numeriVocali2 = 0;
+
+    let vocaliTrovate2 = []
+    
+    for ( let i = 0; i < stringa2.length; i++){
+       if(lettere2.includes(stringa2[i])){
+        numeriVocali2++
+
+        vocaliTrovate2.push(stringa2[i])
+       }
+       
+    }
+    console.log(vocaliTrovate2.length);
+    return numeriVocali2 + " " + vocaliTrovate2;
+    
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 console.log(primaParola(word));
+console.log(primaParola2(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -10,9 +10,31 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+function saluto(nome){
+
+    // inserisco la data e orario corrente
+    const dataDiOggi = new Date();
+    console.log(dataDiOggi);
+
+    // estrago l'oraio preciso
+    const oraCorrente = dataDiOggi.getHours();
+    console.log(oraCorrente);
+    let saluto = "";
+
+    if(oraCorrente <= 13){
+        saluto = "Buongiorno"
+    }else if(oraCorrente <= 17){
+        saluto = "Buon pomeriggio"
+    }else{
+        saluto = "Buonasera"
+    }
+    return saluto + " " + nome;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(saluto(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
