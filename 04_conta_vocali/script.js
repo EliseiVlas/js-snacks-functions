a36dd59c0987bd805b7f224a6793feb740f5faf6
Elisei <eliseivlas.002@gmail.com>
2025-01-09 17:26:24 +0100

    esercizio 4 incompleto

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,20 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
+function primaaParola(stringa){
+    
+    // creeo un arrai vuoto
+    
+
+   
+        return stringa.split('').filter(vocale => 'aeiouAEIOU'.includes(vocale)).length 
+   
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(primaaParola(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
